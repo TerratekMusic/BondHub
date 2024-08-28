f482521285cffb958e6d5d189f8e00ac713c1905
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-08-28 14:37:47 -0600

    abi Multiple token

--- contracts/pTokenBasic.sol
+++ contracts/pTokenBasic.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 
+//0xdfFcf16b20ccb4B81EfDEdaf7a72bE6B16a35041 SEPOLIA KAKAROT
+
 contract MyToken is ERC20, Ownable {
     constructor(address initialOwner)
         ERC20("MyToken", "MTK")
