aef74c0d28705c83a61b6d190159617c5c2040c8
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-09-01 15:58:38 -0600

    Update navbar.tsx

--- Frontend/BondHub/src/Components/navbar.tsx
+++ Frontend/BondHub/src/Components/navbar.tsx
@@ -1,41 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
   const navStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center', // Centrar elementos verticalmente
-    position: 'fixed' as 'fixed', // Especifica la posición como 'fixed'
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center", // Centrar elementos verticalmente
+    position: "fixed" as "fixed", // Especifica la posición como 'fixed'
     top: 10,
+
     left: 30,
     right: 30,
-    height: '60px', // Ajusta la altura según sea necesario
-    backgroundColor: 'transparent', // Hacer transparente el fondo
-    color: 'white',
-    padding: '0', // Eliminar padding
+    height: "60px", // Ajusta la altura según sea necesario
+    backgroundColor: "transparent", // Hacer transparente el fondo
+    color: "white",
+    padding: "0", // Eliminar padding
     zIndex: 1000,
-    boxShadow: 'none' // Eliminar sombra
+    boxShadow: "none", // Eliminar sombra
   };
 
   const linkStyle = {
-    color: 'white',
-    textDecoration: 'none',
-    padding: '0.5rem 1rem',
-    borderRadius: '4px'
+    color: "white",
+    textDecoration: "none",
+    padding: "0.5rem 1rem",
+    borderRadius: "4px",
   };
 
   return (
     <nav style={navStyle}>
-      <div style={{ flexGrow: 0 }}> {/* Sección del logo */}
-        <img src="/src/assets/images/logo.png" alt="Logo" style={{ height: '50px' }} />
+      <div style={{ flexGrow: 0 }}>
+        {" "}
+        {/* Sección del logo */}
+        <img
+          src="/src/assets/images/logo.png"
+          alt="Logo"
+          style={{ height: "50px" }}
+        />
       </div>
-      <div style={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}> {/* Sección de enlaces de navegación */}
-        <Link to="/" style={linkStyle}>Home</Link>
-        <Link to="/Marketlab" style={linkStyle}>Lab</Link>
-        <Link to="/marketplace" style={linkStyle}>Marketplace</Link>
+      <div style={{ flexGrow: 1, display: "flex", justifyContent: "center" }}>
+        {" "}
+        {/* Sección de enlaces de navegación */}
+        <Link to="/" style={linkStyle}>
+          Home
+        </Link>
+        <Link to="/Marketlab" style={linkStyle}>
+          Lab
+        </Link>
+        <Link to="/marketplace" style={linkStyle}>
+          Marketplace
+        </Link>
       </div>
-      <div style={{ flexGrow: 0, color: 'white' }}> {/* Sección de conexión de la billetera */}
+      <div style={{ flexGrow: 0, color: "white" }}>
+        {" "}
+        {/* Sección de conexión de la billetera */}
         Connect Wallet
       </div>
     </nav>
